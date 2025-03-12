142ff24abfb7c262e8d99d00ad7cac6757966a17
dqh999 <dqhit999@gmail.com>
2025-03-12 22:26:20 +0700

    first commit

---
+++ account-service/src/main/java/com/internet/banking/model/entity/AccountEntity.java
@@ -0,0 +1,9 @@
+package com.internet.banking.model.entity;
+
+import java.math.BigDecimal;
+
+public class AccountEntity {
+    private String id;
+    private String accountNumber;
+    private BigDecimal balance;
+}
